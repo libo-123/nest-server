3f35c716aff4153d24626d3bcd03c79d9c371c0e
bo3.li <bo3.li@ximalaya.com>
2024-12-04 16:39:45 +0800

    feat: 添加页面

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './meta/meta';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.hello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import {page} from './page/index'
 @Injectable()
 export class AppService {
   hello(): string {
-    return `Hello man!`;
+    return page();
   }
 }
